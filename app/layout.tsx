4937253769eb48fd33f21f39b23e034e4801ee55
TeukHee <rlaxmrgml@naver.com>
2024-08-17 01:24:42 +0900

    design: add global style theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../styles/reset.css";
+import "../styles/theme.css";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
