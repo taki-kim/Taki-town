231a9a724331462f8ccc5b438355d8b78427739f
TeukHee <rlaxmrgml@naver.com>
2024-09-10 12:14:26 +0900

    config: allow all image path

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http", // HTTP 프로토콜 허용
+        hostname: "**", // 모든 도메인 허용
+      },
+      {
+        protocol: "https", // HTTPS 프로토콜 허용
+        hostname: "**", // 모든 도메인 허용
+      },
+    ],
+  },
+};
 
 export default nextConfig;
