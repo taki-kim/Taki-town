8498e5d4eef05540775f4a80fcb72b0efb27ac34
TeukHee <rlaxmrgml@naver.com>
2024-09-10 17:12:18 +0900

    feat: add fixed color function on button component

--- components/button/main-button.tsx
+++ components/button/main-button.tsx
@@ -4,14 +4,22 @@ import { getButtonSizeClass } from "@/utils/button";
 type MainButtonProps = {
   text: string;
   size: string;
+  color?: string | undefined;
 };
 
-export default function MainButton({ text, size }: MainButtonProps) {
+export default function MainButton({ text, size, color }: MainButtonProps) {
   return (
     <div
       className={`${styles["button-wrapper"]} ${
         styles[getButtonSizeClass(size) as string]
-      }`}
+      } `}
+      style={
+        color === "light"
+          ? { color: "lightgray", border: "1px solid lightgray" }
+          : color === "dark"
+          ? { color: "#1a2140", border: "1px solid #1a2140" }
+          : {}
+      }
     >
       {text}
     </div>
