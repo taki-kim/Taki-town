577a9c0d8a0d6552482cf2f697ddb3a03b8f9f81
TeukHee <rlaxmrgml@naver.com>
2024-09-11 09:27:58 +0900

    feat: add footer

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,4 +1,4 @@
-import HeaderLogo from "./header-logo";
+import MainLogo from "../main-logo/main-logo";
 import HeaderList from "./heade-list";
 import ThemeChangeButton from "./theme-change-button/theme-change-button";
 import styles from "./header.module.css";
@@ -6,7 +6,7 @@ import styles from "./header.module.css";
 export default function Header() {
   return (
     <header className={styles.header}>
-      <HeaderLogo link="/" title="TAKI-TOWN" />
+      <MainLogo link="/" title="TAKI-TOWN" />
       <nav className={styles.navbar}>
         <ul className={styles.navbar}>
           <HeaderList href="/posts" listName="posts" />
