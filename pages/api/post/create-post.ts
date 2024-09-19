e93dc2ad98cef54b5f9f69fa4f4a949cbbd1e176
TeukHee <rlaxmrgml@naver.com>
2024-09-19 15:41:14 +0900

    feat : add to create post function

---
+++ pages/api/post/create-post.ts
@@ -0,0 +1,23 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { connectDatabase } from "../../../utils/db";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  let client;
+  try {
+    client = await connectDatabase();
+    if (req.method === "POST") {
+      const db = client.db(process.env.DB_NAME);
+      const collection = db.collection("posts");
+      await collection.insertOne(req.body);
+    }
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  } finally {
+    res.status(201).json({ message: "your post is inserted" });
+    if (client) client.close();
+  }
+}
