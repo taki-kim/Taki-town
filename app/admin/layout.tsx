e8148d0fee56f25daeda3d11e17729d80e8a1c40
TeukHee <rlaxmrgml@naver.com>
2024-09-19 15:43:04 +0900

    env: change folder structure about admin page

---
+++ app/admin/layout.tsx
@@ -0,0 +1,14 @@
+import styles from "./layout.module.css";
+import PageGuard from "@/components/page-guard/page-guard";
+
+export default function AdminLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className={styles["body"]}>
+      <PageGuard>{children}</PageGuard>
+    </div>
+  );
+}
--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,11 +1,7 @@
-import PageGuard from "@/components/page-guard/page-guard";
-
 export default function Admin() {
   return (
-    <PageGuard>
-      <div>
-        <h1>this is admin</h1>
-      </div>
-    </PageGuard>
+    <div>
+      <h1>this is admin</h1>
+    </div>
   );
 }
