1968aa01111d0c1e508a5f62107ef519cf600954
TeukHee <rlaxmrgml@naver.com>
2024-09-24 02:21:14 +0900

    delete: delete useless code

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,25 +1,3 @@
-"use client";
-
-import { useState, useEffect } from "react";
-
 export default function Post() {
-  const [data, setData] = useState("");
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("/api/post/get-all-posts");
-        if (!response.ok) {
-          throw new Error("Failed to fetch data");
-        }
-        const result = await response.json();
-        setData(result);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-
-    fetchData();
-  }, []);
   return <div></div>;
 }
