334e242f9f7ba364a82af5b21a0e439f70e963b7
TeukHee <rlaxmrgml@naver.com>
2024-09-23 19:26:06 +0900

    feat : add fetching posts function

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,10 @@
 "use client";
 
-import { useState, useEffect } from "react";
 import styles from "./page.module.css";
 import Introduction from "@/components/main-page/introduction";
 import AboutPostSection from "@/components/main-page/about-post-section/about-post-section";
 
 export default function Home() {
-  const [data, setData] = useState<any>("");
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("/api/test");
-        if (!response.ok) {
-          throw new Error("Failed to fetch data");
-        }
-        const result = await response.json();
-        setData(result.name);
-        console.log(result);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
   return (
     <>
       <header className={styles["header"]}>
--- app/posts/layout.tsx
+++ app/posts/layout.tsx
@@ -2,9 +2,14 @@ import styles from "./layout.module.css";
 import PostsHeader from "@/components/posts/header/posts-header";
 import PostList from "@/components/posts/post-list/post-list";
 
-export default function PostLayout() {
+export default function PostLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <div className={styles["wrapper"]}>
+      {children}
       <PostsHeader />
       <PostList />
     </div>
