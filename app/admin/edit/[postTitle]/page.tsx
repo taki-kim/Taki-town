5349f85afe62f905d8a87be70a1d896ddc2dd2dc
TeukHee <rlaxmrgml@naver.com>
2024-10-20 23:29:51 +0900

    fix: fix edit page's fetch problem

--- app/admin/edit/[postTitle]/page.tsx
+++ app/admin/edit/[postTitle]/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { usePathname } from "next/navigation";
 
 import ReactMarkdown from "react-markdown";
