9a5aed5fc6077bbe1cf47a1647adbc5d778cd715
TeukHee <rlaxmrgml@naver.com>
2024-10-30 16:32:12 +0900

    fix: fix deployment bugs

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -3,16 +3,11 @@ import PostsHeader from "@/components/posts/header/posts-header";
 import PostList from "@/components/posts/post-list/post-list";
 import { fetchPostList } from "@/utils/fetchData";
 
-export default async function PostLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default async function Posts() {
   const dataList = await fetchPostList();
 
   return (
     <div className={styles["wrapper"]}>
-      {children}
       <PostsHeader />
       <PostList dataList={dataList} />
     </div>
