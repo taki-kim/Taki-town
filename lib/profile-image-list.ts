4dd3698e009eba995f2dbcbeca787781bf49353e
TeukHee <rlaxmrgml@naver.com>
2025-01-06 16:19:55 +0900

    feat: add comment-profile-image-list

---
+++ lib/profile-image-list.ts
@@ -0,0 +1,26 @@
+const profileImageList = [
+  { imageName: "basic", imageLink: "/image/profile-image/basic.png" },
+  { imageName: "circle", imageLink: "/image/profile-image/circle.jpg" },
+  { imageName: "triangle", imageLink: "/image/profile-image/triangle.jpg" },
+  { imageName: "square", imageLink: "/image/profile-image/square.jpg" },
+  {
+    imageName: "neko_orange",
+    imageLink: "/image/profile-image/neko_orange.jpg",
+  },
+  { imageName: "sunglasses", imageLink: "/image/profile-image/sunglasses.png" },
+  {
+    imageName: "sunglasses_neko",
+    imageLink: "/image/profile-image/sunglasses_neko.jpg",
+  },
+  { imageName: "purple_sky", imageLink: "/image/profile-image/purple_sky.jpg" },
+  { imageName: "blue_sky", imageLink: "/image/profile-image/blue_sky.png" },
+  { imageName: "gintama", imageLink: "/image/profile-image/gintama.png" },
+
+  { imageName: "boochi", imageLink: "/image/profile-image/bocchi.png" },
+  { imageName: "rick", imageLink: "/image/profile-image/rick.png" },
+  { imageName: "morty", imageLink: "/image/profile-image/morty.png" },
+  { imageName: "kaiji", imageLink: "/image/profile-image/kaiji.png" },
+  { imageName: "musashi", imageLink: "/image/profile-image/musashi.png" },
+];
+
+export default profileImageList;
