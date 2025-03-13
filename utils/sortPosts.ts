00eb4fe88f4c3963e57bb48afb7f7e3481640689
TeukHee <rlaxmrgml@naver.com>
2025-03-14 03:58:08 +0900

    fix: fix sort-recent-posts function

--- utils/sortPosts.ts
+++ utils/sortPosts.ts
@@ -1,24 +1,34 @@
-export function sortRecentPosts(data: any[] | any) {
+import { PostDataProps } from "@/type";
+
+export function sortRecentPosts(data: PostDataProps[]) {
   const dateStringToNumber = (date: string) => {
     return Number(date.split("-").join(""));
   };
 
-  const recentSorted = data.sort(
-    (a: any, b: any) => dateStringToNumber(b.date) - dateStringToNumber(a.date)
+  const recentPosts = data.filter(
+    (item: PostDataProps) => item.category !== "development"
+  );
+
+  const recentSorted = recentPosts.sort(
+    (a: PostDataProps, b: PostDataProps) =>
+      dateStringToNumber(b.date) - dateStringToNumber(a.date)
   );
 
   return recentSorted.slice(0, 5);
 }
 
-export function sortRecentDevPosts(data: any[] | any) {
+export function sortRecentDevPosts(data: PostDataProps[]) {
   const dateStringToNumber = (date: string) => {
     return Number(date.split("-").join(""));
   };
 
-  const devPosts = data.filter((item: any) => item.category === "development");
+  const devPosts = data.filter(
+    (item: PostDataProps) => item.category === "development"
+  );
 
   const recentSorted = devPosts.sort(
-    (a: any, b: any) => dateStringToNumber(b.date) - dateStringToNumber(a.date)
+    (a: PostDataProps, b: PostDataProps) =>
+      dateStringToNumber(b.date) - dateStringToNumber(a.date)
   );
 
   return recentSorted.slice(0, 5);
