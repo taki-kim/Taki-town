74a6e8f97a37ce63b09d5fbbb459ef3a5c667996
TeukHee <rlaxmrgml@naver.com>
2025-04-08 00:49:33 +0900

    feat: apply react-query on fetching-comments

--- components/comment/comment-container.tsx
+++ components/comment/comment-container.tsx
@@ -1,14 +1,9 @@
 "use client";
 
-import { Suspense } from "react";
-import { useEffect, useState } from "react";
-
+import React, { Suspense } from "react";
 import styles from "./comment-container.module.css";
 import CommentInput from "./comment-input/comment-input";
 import CommentsList from "./comments-list/comments-list";
-import { CommentProps } from "@/type";
-import { STALE_TIME } from "@/constant";
-import { useCommentRefetch } from "@/hooks/useCommentRefetch";
 
 export type CommentContainerProps = {
   articleTitle: string;
@@ -17,28 +12,12 @@ export type CommentContainerProps = {
 export default function CommentContainer({
   articleTitle,
 }: CommentContainerProps) {
-  // 댓글 불러오기 (postName)
-  const { triggerRefetchCount } = useCommentRefetch();
-  const [commentsList, setCommentsList] = useState<CommentProps[]>([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch(`/api/comment/get/${articleTitle}`, {
-        next: { revalidate: STALE_TIME },
-      });
-      const result = await response.json();
-      if (result) setCommentsList(result);
-    };
-
-    fetchData();
-  }, [triggerRefetchCount]);
-
   return (
     <div className={styles["wrapper"]}>
       <h1 className={styles["title"]}>Comments</h1>
       <CommentInput articleTitle={articleTitle} />
-      <Suspense fallback={<>loading...</>}>
-        <CommentsList commentsList={commentsList} />
+      <Suspense fallback={<>...loading</>}>
+        <CommentsList articleTitle={articleTitle} />
       </Suspense>
     </div>
   );
--- components/comment/comments-list/comments-list.tsx
+++ components/comment/comments-list/comments-list.tsx
@@ -1,17 +1,21 @@
-import { CommentProps } from "@/type";
+"use client";
+
+import useGetComments from "@/hooks/useGetComments";
 import styles from "./comments-list.module.css";
 import CommentCard from "../comment-card/comment-card";
 import { sortByNewestComment } from "@/utils/comment";
 
 type CommentsListProps = {
-  commentsList: Partial<CommentProps>[];
+  articleTitle: string;
   newestFirst?: boolean;
 };
 
 export default function CommentsList({
-  commentsList,
+  articleTitle,
   newestFirst,
 }: CommentsListProps) {
+  const { data: commentsList } = useGetComments(articleTitle);
+
   if (newestFirst) {
     return (
       <div className={styles["wrapper"]}>
