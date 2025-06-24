59169db9284f386b203c3f42be67de842830551e
TeukHee <rlaxmrgml@naver.com>
2025-06-25 03:21:23 +0900

    feat: update main-page-introduction

--- components/main-page/introduction.tsx
+++ components/main-page/introduction.tsx
@@ -1,12 +1,10 @@
 "use client";
 
 import { useEffect, useRef, useState } from "react";
-import Link from "next/link";
-import Image from "next/image";
+import TextAutoSlider from "./text-auto-slider/TextAutoSlider";
 
 import styles from "./introduction.module.scss";
-import MainButton from "../button/main-button";
-import wallpaper from "/public/image/wallpaper.webp";
+import NavButton from "../button/nav-button";
 
 export default function Introduction() {
   const textRef = useRef<HTMLDivElement>(null);
@@ -40,25 +38,22 @@ export default function Introduction() {
   return (
     <>
       <main className={styles["wrapper"]}>
-        <Image src={wallpaper} alt="wallpaper" quality={60} fill priority />
-
         <div
           className={`${styles["text-wrapper"]} ${
             isVisible ? styles["fade-in"] : ""
           }`}
           ref={textRef}
         >
-          안녕하세요 웹 프론트엔드 개발자 Taki입니다. <br />
-          이곳은 저의 개인 블로그, 제가 만든 작업들의 내용과 고민의 흔적들을
-          남기고 기억하는 곳입니다.
+          Web developer Taki,
           <br />
-          저의 작업물들과 경험들, 그리고 흥미롭고 관심이 가는 주제들을 자유롭게
-          기록하고 있습니다.
+          focused on Fast Web, Intuitive UI, and Better UX.
+          <br />
+          <div className={styles["carousel-text-wrapper"]}>
+            Building with <TextAutoSlider />
+          </div>
         </div>
         <div className={styles["button-wrapper"]}>
-          <Link href="/about" className={styles["nav-button"]}>
-            <MainButton text="About Me" size="medium" color="light" />
-          </Link>
+          <NavButton text="About Me" size="medium" link="/about" />
         </div>
       </main>
     </>
---
+++ components/main-page/text-auto-slider/TextAutoSlider.tsx
@@ -0,0 +1,78 @@
+import React, { useEffect, useRef, useState } from "react";
+import styles from "./TextAutoSlider.module.scss";
+import NextIcon from "/public/icons/nextjs-icon.svg";
+import ReactIcon from "/public/icons/react-icon.svg";
+import TypeScriptIcon from "/public/icons/typescript-icon.svg";
+import MongoDBIcon from "/public/icons/mongodb-icon.svg";
+
+const techStack = [
+  { stack: "Next.js", icon: NextIcon },
+  { stack: "React.js", icon: ReactIcon },
+  { stack: "TypeScript", icon: TypeScriptIcon },
+  { stack: "MongoDB", icon: MongoDBIcon },
+];
+export default function TextAutoSlider() {
+  const extendedItems = [
+    techStack[techStack.length - 1],
+    ...techStack,
+    techStack[0],
+  ];
+  const [index, setIndex] = useState(1);
+  const [transition, setTransition] = useState(true);
+  const timerRef = useRef<NodeJS.Timeout | null>(null);
+
+  useEffect(() => {
+    timerRef.current = setInterval(() => {
+      setIndex((prev) => prev + 1);
+      setTransition(true);
+    }, 3000);
+    return () => {
+      if (timerRef.current) clearInterval(timerRef.current);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (index === extendedItems.length - 1) {
+      setTimeout(() => {
+        setTransition(false);
+        setIndex(1);
+      }, 800);
+    }
+    if (index === 0) {
+      setTimeout(() => {
+        setTransition(false);
+        setIndex(extendedItems.length - 2);
+      }, 800);
+    }
+  }, [index, extendedItems.length]);
+
+  useEffect(() => {
+    if (!transition) {
+      setTimeout(() => setTransition(true), 50);
+    }
+  }, [transition]);
+
+  return (
+    <div className={styles["wrapper"]}>
+      <div
+        className={styles["inner-wrapper"]}
+        style={{
+          transition: transition
+            ? "transform 0.3s cubic-bezier(0.4,0,0.2,1)"
+            : "none",
+          transform: `translateY(-${index * 40}px)`,
+        }}
+      >
+        {extendedItems.map((item, i) => {
+          const Icon = item.icon;
+          return (
+            <div key={i} className={styles["text-wrapper"]}>
+              {item.stack}
+              <Icon style={{ color: "var(--color-primary)" }} />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
