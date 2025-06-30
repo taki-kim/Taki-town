5278d65b928f168aeb37b8d4503ca293cd6241a4
TeukHee <rlaxmrgml@naver.com>
2025-06-30 16:44:20 +0900

    feat: add floating-effect hook

---
+++ hooks/useFloatingEffect.ts
@@ -0,0 +1,35 @@
+"use client";
+
+import { useState, useEffect, useRef } from "react";
+
+export default function useFloatingEffect<T extends HTMLElement>() {
+  const [isVisible, setIsVisible] = useState(false);
+  const targetRef = useRef<T>(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setIsVisible(true);
+          }
+        });
+      },
+      { threshold: 0.2 }
+    );
+
+    const currentTarget = targetRef.current;
+
+    if (currentTarget) {
+      observer.observe(currentTarget);
+    }
+
+    return () => {
+      if (currentTarget) {
+        observer.unobserve(currentTarget);
+      }
+    };
+  }, []);
+
+  return { isVisible, targetRef };
+}
