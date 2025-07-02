9012be9d92a00fd679f16eaae9b4b0856cb9b85d
TeukHee <rlaxmrgml@naver.com>
2025-07-02 20:09:22 +0900

    feat: add strength-section on about-page

---
+++ components/about/strength-section/StrengthSection.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import styles from "./StrengthSection.module.scss";
+import { strengthList } from "@/lib/about";
+import StrengthCard from "./strength-card/StrengthCard";
+import useFloatingEffect from "@/hooks/useFloatingEffect";
+
+export default function StrengthSection() {
+  const { isVisible, targetRef } = useFloatingEffect<HTMLDivElement>();
+  return (
+    <div
+      className={`${styles["wrapper"]} ${isVisible ? styles.visible : ""}`}
+      ref={targetRef}
+    >
+      <h1 className={styles["title"]}>Focusing On</h1>
+      <div className={styles["cards-list"]}>
+        {strengthList.map((e) => (
+          <StrengthCard
+            key={e.title}
+            image={e.image}
+            title={e.title}
+            description={e.description}
+            ref={targetRef}
+            className={`${styles.card} ${isVisible ? styles.visible : ""}`}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
---
+++ lib/about.ts
@@ -0,0 +1,20 @@
+export const strengthList = [
+  {
+    image: "/image/about/strength_ux.jpg",
+    title: "쾌적한 사용자 경험",
+    description:
+      "세심한 관심과 개선이 만족스러운 UX를 만든다고 믿습니다. 정량적 지표를 바탕으로 UX를 개선하고, 사용자가 불편을 느끼지 않는 쾌적한 경험을 만들어갑니다.",
+  },
+  {
+    image: "/image/about/strength_ui.jpg",
+    title: "신뢰할 수 있는 UI 품질",
+    description:
+      "UI는 서비스의 얼굴이라 생각합니다. 예상 가능한 동작, 일관된 스타일 등을 고려해 완성도 높은 UI를 구현합니다. 디자인 시스템과 코드 관리를 통해 체계적으로 품질을 유지합니다.",
+  },
+  {
+    image: "/image/about/strength_dx.jpg",
+    title: "견고한 개발 환경",
+    description:
+      "견고한 개발 환경이 결국 제품의 완성도로 이어진다고 믿습니다. 유지보수가 쉬운 구조, 직관적인 코드, 유연한 컴포넌트 설계를 통해 협업과 확장성을 고려한 환경을 만들어갑니다.",
+  },
+];
